d20d90d4be7207a2af2d707eb2b67060f99c801d
Vittorio Zuccala <vittorio.zuccala@gmail.com>
2022-12-21 11:13:20 +0100

    Ulteriori modifiche al GEUTY400F

--- R/estrai_tabella_geuty400f.R
+++ R/estrai_tabella_geuty400f.R
@@ -1,9 +1,13 @@
 #' Estrae GEUTY
 #'
 #' Questa funzione permette di estrarre la tabella GEUTY400F dal CRM aziendale su AS400
+#' 
 #' La elaborazione dura almeno un paio di minuti perchè deve estrarre 
 #' centinaia di migliaia di records.
+#' 
 #' Al termine viene estratto un dataframe sottoforma di tibble.
+#' 
+#' I campi numerici sono stati modificati correttamente in numero
 #' @param connessione è la connessione ad AS400 di solito realizzata con la funzione connessione_as400 del pacchetto.
 #' @return Ritorna geuty
 #' @examples 
@@ -22,26 +26,7 @@ estrai_tabella_geuty400f <- function(connessione){
   
   names(tb) <- names(tb) %>% map_chr(str_replace,"GE400_","")
   
-  tb <- tb %>% mutate(
-    across(
-      .cols = c(
-        PRATICA, STATO, STATO_WF, BANCA,
-        NUM_RATE, IMP_RATE,
-        MONTANTE, FINANZIATO, FINANZIATO_CON_ASS, DELTA_MONTANTE,
-        COSTO_1, COSTO_2, COSTO_3, COSTO_4, COSTO_5, COSTO_6, 
-        COSTO_ASS_VITA, COSTO_ASS_2,
-        NETTO, IMPEGNI, INTER_SCALARI, COMM_BANCARIE, BOLLO_RIVALSA,
-        PROVVIGIONI_TOT, PROVVIGIONI_AGE, PROVVIGIONI_SUB,
-        SPESE_DA_RETROCEDERE, REDDITO, SALDO_CLIENTE, 
-        TAN, TAEG, TEG, 
-        IRR_NOMIN, IRR_EFFET,
-        TOT_RIL_CARIC, 
-        IMP_FLUSSO, TASSO_FLU
-      ), 
-      .fns =  as.numeric
-    )
-  )
-  
+
   tb$DENOM_1_ANA_6 <- riprogramma_testo(tb$DENOM_1_ANA_6)
   tb$DENOM_1_ANA_1 <- riprogramma_testo(tb$DENOM_1_ANA_1)
   
@@ -69,8 +54,7 @@ estrai_tabella_geuty400f <- function(connessione){
   # Questa istruzione serve per aggiustare l'encoding
   tb %>% mutate_if(is.character, ~gsub('[^ -~]', '', .))
   
-  
-  
+
   unique(StatoAttributoPratica) %>% 
     str_match("RT$")
   
@@ -78,6 +62,27 @@ estrai_tabella_geuty400f <- function(connessione){
   for (i in names(tb)) {
     tb[[i]]<-stri_enc_toascii(tb[[i]])
   }
+  
+  # Modifico i campi che sono numerici, forzando il tipo di dati
+  tb <- tb %>% mutate(
+    across(
+      c(
+        PRATICA, STATO, STATO_WF, BANCA,
+        NUM_RATE, IMP_RATE,
+        MONTANTE, FINANZIATO, FINANZIATO_CON_ASS, DELTA_MONTANTE,
+        COSTO_1, COSTO_2, COSTO_3, COSTO_4, COSTO_5, COSTO_6, 
+        COSTO_ASS_VITA, COSTO_ASS_2,
+        NETTO, IMPEGNI, INTER_SCALARI, COMM_BANCARIE, BOLLO_RIVALSA,
+        PROVVIGIONI_TOT, PROVVIGIONI_AGE, PROVVIGIONI_SUB,
+        SPESE_DA_RETROCEDERE, REDDITO, SALDO_CLIENTE, 
+        TAN, TAEG, TEG, 
+        IRR_NOMIN, IRR_EFFET,
+        TOT_RIL_CARIC, 
+        IMP_FLUSSO, TASSO_FLU
+      ), 
+      as.numeric
+    )
+  )
   
   return(tb)
 }
